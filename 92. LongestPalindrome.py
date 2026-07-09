512a8266666ca0af1f08ae1a6fa61e0fb3b24e0b
TejaswiniAllu <tejaswiniallu6@gmail.com>
2026-07-09 20:31:40 +0530

    Create 92. LongestPalindrome.py

---
+++ 92. LongestPalindrome.py
@@ -0,0 +1,9 @@
+print("Enter String:")
+s = input()
+longest = ""
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        sub = s[i:j+1]
+        if sub == sub[::-1] and len(sub) > len(longest):
+            longest = sub
+print("Longest Palindrome:", longest)
